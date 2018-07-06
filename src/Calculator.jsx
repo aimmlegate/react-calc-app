fe0a6c1bf4b11686b31758b612b537f229c650af
Andrey Lukin <aimm.legate@gmail.com>
2018-07-06 11:10:40 +0500

    story-2/3

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -3,36 +3,91 @@ import { Button } from 'reactstrap';
 import './assets/Calculator.css';
 
 class Calculator extends Component {
+  state = { screen: '0', calcState: 'READY' };
+
+  pressHandlerAC = () => {};
+  pressHandlerPM = () => {};
+  pressHandlerOperand = () => {};
+  pressHandler = val => () => {
+    switch (this.state.calcState) {
+      case 'READY':
+        this.setState({ screen: `${val}`, calcState: 'INPUT' });
+        break;
+      case 'INPUT':
+        this.setState({ screen: `${this.state.screen}${val}` });
+        break;
+      default:
+        console.error('wtf');
+    }
+  };
   render() {
     return (
       <div className="app-wrapper">
         <div className="app-container">
           <div className="alert alert-dark return" role="alert">
-            123456789012
+            {this.state.screen}
           </div>
           <div className="calc-layout">
-            <Button color="secondary">AC</Button>
-            <Button color="secondary">+/-</Button>
-            <Button color="secondary">%</Button>
-            <Button color="warning">/</Button>
-            <Button color="secondary">7</Button>
-            <Button color="secondary">8</Button>
-            <Button color="secondary">9</Button>
-            <Button color="warning">x</Button>
-            <Button color="secondary">4</Button>
-            <Button color="secondary">5</Button>
-            <Button color="secondary">6</Button>
-            <Button color="warning">-</Button>
-            <Button color="secondary">1</Button>
-            <Button color="secondary">2</Button>
-            <Button color="secondary">3</Button>
-            <Button color="warning">+</Button>
-            <Button color="secondary zero-btn">0</Button>
-            <Button color="secondary">,</Button>
-            <Button color="warning">=</Button>
+            <Button
+              color="secondary"
+              onClick={this.pressHandlerAC}>{(this.state.calcState === 'INPUT') ? 'C' : 'AC'}</Button>
+            <Button
+              color="secondary"
+              onClick={this.pressHandlerPM('+/-')}>+/-</Button>
+            <Button
+              color="secondary"
+              disabled>%</Button>
+            <Button
+              color="warning"
+              onClick={this.pressHandlerOperand('/')}>/</Button>
+            <Button
+              color="secondary"
+              onClick={this.pressHandler('7')}>7</Button>
+            <Button
+              color="secondary"
+              onClick={this.pressHandler('8')}>8</Button>
+            <Button
+              color="secondary"
+              onClick={this.pressHandler('9')}>9</Button>
+            <Button
+              color="warning"
+              onClick={this.pressHandlerOperand('x')}>x</Button>
+            <Button
+              color="secondary"
+              onClick={this.pressHandler('4')}>4</Button>
+            <Button
+              color="secondary"
+              onClick={this.pressHandler('5')}>5</Button>
+            <Button
+              color="secondary"
+              onClick={this.pressHandler('6')}>6</Button>
+            <Button
+              color="warning"
+              onClick={this.pressHandlerOperand('-')}>-</Button>
+            <Button
+              color="secondary"
+              onClick={this.pressHandler('1')}>1</Button>
+            <Button
+              color="secondary"
+              onClick={this.pressHandler('2')}>2</Button>
+            <Button
+              color="secondary"
+              onClick={this.pressHandler('3')}>3</Button>
+            <Button
+              color="warning"
+              onClick={this.pressHandlerOperand('+')}>+</Button>
+            <Button
+              color="secondary zero-btn"
+              onClick={this.pressHandler('0')}>0</Button>
+            <Button
+              color="secondary"
+              onClick={this.pressHandlerOperand('.')}>.</Button>
+            <Button
+              color="warning"
+              onClick={this.pressHandlerSumm}>=</Button>
           </div>
         </div>
-      </div> 
+      </div>
     );
   }
 }
